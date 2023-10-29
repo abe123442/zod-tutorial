841ef451a3b9ba8a6f55c4bcc791e48e43a68240
Abraham <ae248abraham@gmail.com>
2023-10-29 17:22:32 +1100

    Completed 04-infer

--- src/04-infer.problem.ts
+++ src/04-infer.problem.ts
@@ -10,8 +10,9 @@ const StarWarsPeopleResults = z.object({
   results: z.array(StarWarsPerson),
 });
 
-const logStarWarsPeopleResults = (data: unknown) => {
-  //                                    ^ 🕵️‍♂️
+type Results = z.infer<typeof StarWarsPeopleResults>;
+
+const logStarWarsPeopleResults = (data: Results) => {
   data.results.map((person) => {
     console.log(person.name);
   });
